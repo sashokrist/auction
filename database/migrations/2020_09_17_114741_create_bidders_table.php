46e349a238c6c72af759fc57e39a7b4fa9353b79
vagrant <vagrant@homestead>
2020-09-17 12:01:59 +0000

    first commit

---
+++ database/migrations/2020_09_17_114741_create_bidders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBiddersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bidders', function (Blueprint $table) {
+            $table->id();
+            $table->string('lastname');
+            $table->string('firstname');
+            $table->string('address');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bidders');
+    }
+}
